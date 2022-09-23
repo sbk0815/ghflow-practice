441d74ad74338dbf225088f3bb9836c1f3d2ee17
sbk0815 <zxcoon77@gmail.com>
2022-09-23 15:17:07 +0900

    feat: Print fizzbuzz
    Condition

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
-for i in range(1, 24+1):
-    if i%5==0:
+for i in range(1, 20+1):
+    if i%15==0:
+        print('fizzbuzz')
+    elif i%5==0:
         print('buzz')
     elif i%3==0:
         print('fizz')
-    elif i%15==0:
-        print('fizzbuzz')
     else:
         print(i)
 
